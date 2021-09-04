e113422201961f97eca3cb849cf3f3e7ab958ab1
Andrea Tosato <andrea.tosato@hotmail.it>
2021-09-04 09:04:44 +0200

    Fix Form insert

--- src/Dapr.Osservability.Sample/src/Frontend/osservability-client/src/app/writer/writer.component.ts
+++ src/Dapr.Osservability.Sample/src/Frontend/osservability-client/src/app/writer/writer.component.ts
@@ -13,7 +13,7 @@ import { FruitVegetables, FruitVegetableType } from '../reader/reader.model';
 export class WriterComponent implements OnInit {
   public FruitVegetable: FruitVegetables = new FruitVegetables();
   frmFruitVegetable!: FormGroup;
-  date!: NgbDateStruct;
+  dateSelected!: NgbDateStruct;
   states = [
     {name: 'Fruit', value: 0},
     {name: 'Vegetable', value: 1}
@@ -27,16 +27,24 @@ export class WriterComponent implements OnInit {
       name: ['', Validators.required],
       description: ['', Validators.required],
       quantity: [1, Validators.min(1)],
-      type: FruitVegetableType.Fruit  
+      type: FruitVegetableType.Fruit,
+      expirationDate: this.dateSelected 
     });  
   }
 
   onSubmit(){
-    console.log(this.frmFruitVegetable.value);
-    console.log(this.date);
+    this.FruitVegetable = {
+      expirationDate: new Date(Date.now()), //this.dateSelected.year === null ? new Date(Date.now()) : new Date(this.dateSelected.year, this.dateSelected.month, this.dateSelected.day),
+      temperature: this.frmFruitVegetable.value.temperature,
+      name: this.frmFruitVegetable.value.name,
+      description: this.frmFruitVegetable.value.description,
+      quantity: this.frmFruitVegetable.value.quantity,
+      type: this.frmFruitVegetable.value.type.value
+    };
+    console.log(this.FruitVegetable);
 
-    this.client.post(environment.baseUrl + '/a/FruitVegetables', this.frmFruitVegetable.value)
-    .subscribe(t => console.log(t));
+    this.client.post(environment.baseUrl + '/a/FruitVegetables', this.FruitVegetable)
+      .subscribe(t => console.log(t));
   }
 
 }
